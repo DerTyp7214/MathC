e841ee88bc812a0caa51cc83435ba34b7a053de8
PahaAkos <paha.akos@gmail.com>
2025-06-11 22:13:59 +0200

    Switch to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,63 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "de.dertyp7214.mathc"
+    buildToolsVersion = "36.0.0"
+    compileSdk = 36
+    ndkVersion = "28.1.13356709"
+
+    defaultConfig {
+        minSdk = 23
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+        @Suppress("UnstableApiUsage") externalNativeBuild {
+            cmake {
+                cppFlags("")
+            }
+        }
+    }
+
+    testOptions {
+        targetSdk = 36
+    }
+
+    lint {
+        targetSdk = 36
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+        }
+    }
+    externalNativeBuild {
+        cmake {
+            path("src/main/cpp/CMakeLists.txt")
+            version = "4.0.2"
+        }
+    }
+
+    kotlin {
+        compilerOptions {
+            jvmToolchain(23)
+            jvmTarget.set(JvmTarget.JVM_23)
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.espresso.core)
+}
